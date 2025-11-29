418e86777fb96eeabd3c3708087bf25a3cd27f55
Andy McCoy <andy.k.mccoy@gmail.com>
2025-11-28 21:39:04 -0800

    add persistance api

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,7 @@ import App from './App.vue'
 const app = createApp(App)
 app.use(createPinia())
 app.mount('#app')
+
+if (navigator.storage && navigator.storage.persist) {
+    navigator.storage.persist()
+}
